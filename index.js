5a8ec5c65b6df7e4a62e7786dfb4fcea3fcb18f3
Ben Robinson <ben@click-on.co.uk>
2023-03-19 12:51:48 +0000

    Changed it to run on http port 3000

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
-var https = require('https');
-var fs = require('fs');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
 app.get('/api/whoami', function (req, res) {
   res.json({
     ipaddress:req.headers["host"], 
@@ -30,10 +19,6 @@ app.get('/api/whoami', function (req, res) {
   });
 });
 
-const options = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
-}
-
-const PORT = process.env.PORT || 443
-const listener = https.createServer(options, app).listen(PORT, console.log(`Node.js listening on port  ${PORT}`))
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+});
